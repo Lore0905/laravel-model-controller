28ce646a5e4bb98bb5481976f81d4ee9623a8c31
Lore0905 <lore.castelli4@gmail.com>
2022-02-11 16:48:03 +0100

    BONUS DONE

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -1,29 +1,35 @@
-
-<section>
-    <div class="card">
-        <ul>
-            @foreach ($movies as $movie)
-                <li>
-                    Titolo: {{$movie->title}}
-                </li>
-                <li>
-                    Original title: {{$movie->original_title}}
-                </li>
-                <li>
-                    Nationality: {{$movie->nationality}}
-                </li>
-                <li>
-                    Date: {{$movie->date}}
-                </li>
-                <li>
-                    Vote: {{$movie->vote}}
-                </li>
-                <br>
-                <br>
-                <br>
-                
-                
-            @endforeach
-        </ul>
-    </div>
-</section>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    <title>Document</title>
+</head>
+<body>
+    <section>
+        @foreach ($movies as $movie)
+            <div class="card">
+                <ul>
+                    <li>
+                        Titolo: {{$movie->title}}
+                    </li>
+                    <li>
+                        Original title: {{$movie->original_title}}
+                    </li>
+                    <li>
+                        Nationality: {{$movie->nationality}}
+                    </li>
+                    <li>
+                        Date: {{$movie->date}}
+                    </li>
+                    <li>
+                        Vote: {{$movie->vote}}
+                    </li>
+                </ul>
+            </div>
+        @endforeach
+    </section>
+</body>
+</html>
