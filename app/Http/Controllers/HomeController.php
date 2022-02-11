1ed17d774c6368b31a6762bf9e8cfa37f530370f
Lore0905 <lore.castelli4@gmail.com>
2022-02-11 16:23:23 +0100

    DONE

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -10,7 +10,9 @@ class HomeController extends Controller
     public function index(){
         $Movies = Movie::all();
 
-        dd($Movies);
-        return (view('homepage'));
+        $data = [
+            'movies' => $Movies
+        ];
+        return (view('homepage', $data));
     }
 }
--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -1,10 +1,29 @@
 
 <section>
-    <div>
+    <div class="card">
         <ul>
-            <li>
-                test
-            </li>
+            @foreach ($movies as $movie)
+                <li>
+                    Titolo: {{$movie->title}}
+                </li>
+                <li>
+                    Original title: {{$movie->original_title}}
+                </li>
+                <li>
+                    Nationality: {{$movie->nationality}}
+                </li>
+                <li>
+                    Date: {{$movie->date}}
+                </li>
+                <li>
+                    Vote: {{$movie->vote}}
+                </li>
+                <br>
+                <br>
+                <br>
+                
+                
+            @endforeach
         </ul>
     </div>
 </section>
